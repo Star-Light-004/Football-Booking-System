0102e82591fc778edd7073c6926b659ca6bc0cd3
Bac <truongdinhbac445@gmail.com>
2026-06-19 10:29:09 +0700

    fix url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,23 +1,21 @@
-from django.urls import path, include
-
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve 
 
 urlpatterns = [
-
     path('api/', include('apps.fields.urls')),
-    path('api/users/', include('apps.users.urls')), 
+    path('api/users/', include('apps.users.urls')),
     path('api/bookings/', include('apps.bookings.urls')),
     path('api/reviews/', include('apps.reviews.urls')),
     path('api/timeslots/', include('apps.timeslots.urls')),
     path('api/services/', include('apps.servicesadd.urls')),
     path('api/notifications/', include('apps.notifications.urls')),
-
 ]
 
-
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+# 👇 FIX MEDIA
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
