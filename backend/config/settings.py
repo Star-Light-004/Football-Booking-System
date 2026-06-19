af589b57205bc5ee9e2382d87f402989438ed8ce
Bac <truongdinhbac445@gmail.com>
2026-06-19 10:18:53 +0700

    fix setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,6 +144,6 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'backend' / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
